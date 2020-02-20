e443f9f3417baf606df304a2be256eff102bdd8d
sinsys <nicholas.hazel@gmail.com>
2020-02-20 11:09:27 -0800

    Modified test

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,11 +2,11 @@ const app = require('../src/app');
 
 describe('App', () => {
 
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200 containing "GET request received"', () => {
     return (
       supertest(app)
         .get('/')
-        .expect(200, 'Hello, world!')
+        .expect(200, 'GET request received')
     );
   });
 
